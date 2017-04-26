1f49bc16fdd58a5e2578ce9a14141f46eb44bc07
rhys5690 <rhys5690@gmail.com>
2017-04-26 18:26:03 +1000

    began react

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,7 @@
 // let's go!
-alert('yeah!!');
+import React from "react"; //Loads react library into react
+import { render } from "react-dom"; //imports one method from react DOM
+import StorePicker from "./components/StorePicker"  //.js on the end is assumed
+
+
+render(<StorePicker/>, document.querySelector('#main'));
