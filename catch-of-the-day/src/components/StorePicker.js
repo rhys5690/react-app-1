edfc5db458c431ca95b6284e594ca8cc65fd3c0c
rhys5690 <rhys5690@gmail.com>
2017-04-28 16:48:02 +1000

    worked out the problem

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,13 +3,21 @@ import { getFunName } from "../helpers";
 
 
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log(this.storeInput.value);
+    //First grab text from box
+    //transition from / to /store/
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
-        <button type="submit">Visit Store -></button>
-      </form> //Uses JSX instead of javascript
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
+        <button type="submit">Visit Store </button>
+      </form>
     )
   }
 }
