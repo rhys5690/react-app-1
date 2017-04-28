c654e9466032c8ac228907daca391564e59fee5e
rhys5690 <rhys5690@gmail.com>
2017-04-28 10:53:24 +1000

    progress with react app

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,9 @@
 // let's go!
 import React from "react"; //Loads react library into react
 import { render } from "react-dom"; //imports one method from react DOM
-import StorePicker from "./components/StorePicker"  //.js on the end is assumed
+import "./css/style.css";
+import App from "./components/App";
+import StorePicker from "./components/StorePicker";  //.js on the end is assumed
 
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
