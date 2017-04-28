23f9e8a8adddb6f99bbc50b61a5e7fd524cbaeef
rhys5690 <rhys5690@gmail.com>
2017-04-28 11:31:45 +1000

    .

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,24 @@
 // let's go!
 import React from "react"; //Loads react library into react
 import { render } from "react-dom"; //imports one method from react DOM
+import { BrowserRouter, Match, Miss } from "react-router";
+
 import "./css/style.css";
+import NotFound from "./components/NotFound";
 import App from "./components/App";
 import StorePicker from "./components/StorePicker";  //.js on the end is assumed
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
-render(<App/>, document.querySelector('#main'));
+
+render(<Root/>, document.querySelector('#main'));
