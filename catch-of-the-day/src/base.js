c83aa548b2f84ace01ed1ee22509072e7e3240f5
rhys5690 <rhys5690@gmail.com>
2017-05-07 10:41:31 +1000

    added websocket

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from "re-base";
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCR8O33N1FcZEi6s_u9qJtAW-y-PUxDmqk",
+  authDomain: "catch-of-the-day-rhys-diab.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-rhys-diab.firebaseio.com"
+})
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from "./Order";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
 import Inventory from "./Inventory"
+import base from "../base";
 
 
 class App extends React.Component {
@@ -19,6 +20,17 @@ class App extends React.Component {
       order: {}
 
     };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
   addFish(fish) {
     //update our state
