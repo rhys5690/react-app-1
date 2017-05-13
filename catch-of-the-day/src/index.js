be1edbb2bdaa56db65522af1480742a039dfa2bf
rhys5690 <rhys5690@gmail.com>
2017-05-13 16:06:22 +1000

    added propTypes

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -7,8 +7,6 @@ import App from './components/App';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
 
-// const repo = `/${window.location.pathname.split('/')[1]}`;
-
 const Root = () => {
   return (
     <BrowserRouter>
