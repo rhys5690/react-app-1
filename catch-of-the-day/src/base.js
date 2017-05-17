8a86dd89f642c6ef972699e724c76c4f5a27bb66
rhys5690 <rhys5690@gmail.com>
2017-05-17 15:31:49 +1000

    fixed facebook log in

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  apiKey: "AIzaSyCdgXxyNqCZEeY36Ss9i4jFMSUh7FdGi6M",
-  authDomain: "catch-of-the-day-wes-bos.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos.firebaseio.com",
+  apiKey: "AIzaSyCR8O33N1FcZEi6s_u9qJtAW-y-PUxDmqk",
+  authDomain: "catch-of-the-day-rhys-diab.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-rhys-diab.firebaseio.com"
 });
 
 export default base;
